43d97191a309f9f9b6d5965a53871ab3805d6c28
Basant <chaudashi@gmail.com>
2023-02-02 15:41:07 +0530

    First commit1

---
+++ Linkedlist.java
@@ -0,0 +1,51 @@
+class Linkedlist{
+Node head;
+class Node{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data =d;
+        next=null;
+    }
+}
+public static Linkedlist insert (Linkedlist list, int data)
+{
+    Node new_node= new Node(data);
+    
+    if(list.head==null)
+    {
+        list.head=new_node;
+    }
+    else{
+        Node last=list.head;
+        while(last.next!=null)
+        {
+            last=last.next;
+        }
+        last.next = new_node;
+    }
+    return list;
+
+}
+public static void print(Linkedlist list)
+{
+    Node currNode=list.head;
+    System.out.print("Linkedlist: ");
+    while(currNode !=null)
+    {
+        System.out.print(currNode.data+" ");
+        currNode = currNode.next;
+    }
+}
+public static void main(String args[])
+{
+    Linkedlist list= new Linkedlist();
+    list=insert(list,1);
+    list=insert(list,2);
+    list=insert(list,3);
+    list=insert(list,4);
+     print(list);
+
+}
+}
